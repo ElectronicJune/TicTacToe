fdd82b2920e7e5240c0ce6cfed0cd906370a6f1c
EJune <ejunef09@gmail.com>
2022-09-26 22:36:20 +0800

    add end='' at print_tictactoe func

--- display.py
+++ display.py
@@ -3,5 +3,5 @@ def print_tictactoe(code):
 -----+-----+-----
   {}  |  {}  |  {}  
 -----+-----+-----
-  {}  |  {}  |  {} '''
-    print(grid.format(code[0],code[1],code[2],code[3],code[4],code[5],code[6],code[7],code[8]))
+  {}  |  {}  |  {}  '''
+    print(grid.format(code[0],code[1],code[2],code[3],code[4],code[5],code[6],code[7],code[8]),end='')
