f36b21813857d5d71dac8654cf3175189eab8dad
EJune <ejunef09@gmail.com>
2022-09-26 21:56:22 +0800

    add function to print tic tac toe grid

---
+++ display.py
@@ -0,0 +1,7 @@
+def print_tictactoe(code):
+    grid = '''  {}  |  {}  |  {}  
+-----+-----+-----
+  {}  |  {}  |  {}  
+-----+-----+-----
+  {}  |  {}  |  {} '''
+    print(grid.format(code[0],code[1],code[2],code[3],code[4],code[5],code[6],code[7],code[8]))
