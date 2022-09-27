027a7259e11a574d8d47ab27d7853c001e5e7399
EJune <ejunef09@gmail.com>
2022-09-27 22:20:17 +0800

    done game loop structure

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import display 
+import rules
+import gameplay_history
+import brain
 #print title 
 display.print_title()
 
+# piece info 
+rules.player_piece = input('Choose a piece [O/X]: ')
+rules.bot_piece = 'X' if rules.player_piece!='X' else 'O'
+
 #start outer game loop 
-#   get player peice option
-#   inner game loop
+run_game = True
+game_round = 0
+
+while run_game :
+    game_round += 1
+    print('ROUND',game_round)
+    game_condition = '123456789'
+    
+    if game_round%2 == 0 :
+        # code 
+        pass
+    #inner game loop
+    for i in range(9):
+        display.print_tictactoe(game_condition)
+        #code
+        pass
+
+    run_game = False if input('Play again? [Y/n]: ').lower()=='n' else True
+
 
