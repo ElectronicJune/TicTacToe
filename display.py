edb26a169816c5ebf212a5d2641e359fd5f13d11
EJune <ejunef09@gmail.com>
2022-09-27 21:41:27 +0800

    add function for printing title

--- display.py
+++ display.py
@@ -5,3 +5,8 @@ def print_tictactoe(code):
 -----+-----+-----
   {}  |  {}  |  {}  '''
     print(grid.format(code[0],code[1],code[2],code[3],code[4],code[5],code[6],code[7],code[8]),end='')
+
+def print_title():
+  print('+---------------------------+')
+  print('| TIC TAC TOE GAME with bot |')
+  print('+---------------------------+\n')
