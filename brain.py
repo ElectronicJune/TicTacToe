13276700d60b2dbc77c1d72a0e31ab86ef0b8ac2
EJune <ejunef09@gmail.com>
2022-09-28 22:04:07 +0800

    add react() for brain.py to react game conditions

--- brain.py
+++ brain.py
@@ -1,9 +1,10 @@
 import numpy as np
+from numpy import random
 
 class memory :
     game_code = np.array(['123456789'])
     game_choice = np.array(['1','2','3','4','5','6','7','8','9'],ndmin=2)
-    game_choice_p = np.array([1/9 for i in range(9)])
+    game_choice_p = np.array([1/9 for i in range(9)],ndmin=2)
     def possible_position(self,code):
         arr = [c for c in code if c in '123456789' ]
         return arr + ['0' for i in range(9-len(arr))]
@@ -15,3 +16,7 @@ class memory :
             p = [i/len(p) for i in p]
             p += [0 for i in range(9-len(p))]
             self.game_choice_p = np.vstack((self.game_choice_p,p))
+    def react(self,code):
+        self.add(code)
+        index = np.where(self.game_code==code)[0][0]
+        return random.choice(self.game_choice[index],p=self.game_choice_p[index],size=1)[0]
