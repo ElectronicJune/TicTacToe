4a050ee85bcc8eeaaf694650416603f056d5d249
EJune <ejunef09@gmail.com>
2022-09-28 20:11:15 +0800

    add add() func to store game codes

--- brain.py
+++ brain.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 class memory :
-    game_code = np.array([],ndmin=1)
-    game_choice = np.array([],ndmin=2)
-    game_choice_p = np.array([],ndmin=1)
+    game_code = np.array(['123456789'])
+    game_choice = np.array(['1','2','3','4','5','6','7','8','9'],ndmin=2)
+    game_choice_p = np.array([1/9 for i in range(9)])
+    def possible_position(self,code):
+        arr = [c for c in code if c in '123456789' ]
+        return arr + ['0' for i in range(9-len(arr))]
+    def add(self,code):
+        if code not in self.game_code :
+            self.game_code = np.concatenate((self.game_code,[code]))
+            self.game_choice = np.vstack((self.game_choice,self.possible_position(code)))
+            p = [1 for i in code if i in '123456789']
+            p = [i/len(p) for i in p]
+            p += [0 for i in range(9-len(p))]
+            self.game_choice_p = np.vstack((self.game_choice_p,p))
