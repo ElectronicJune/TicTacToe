b63d1bc9560cb5961829bf0983e4744cc6a80256
EJune <ejunef09@gmail.com>
2022-09-29 21:05:25 +0800

    update is_gameover() to return False when draw

--- rules.py
+++ rules.py
@@ -7,8 +7,12 @@ def is_gameover(code):
             return True
     if code[0] == code[4] == code[8] or code[2] == code[4] == code[6] :
         return True
+    for i in code:
+        if i.isdigit() :
+            break
+    else:
+        return True
     return False
-
 def winner(code):
     for i in range(3):
         if code[i] == code[i+3] == code[i+6]:
