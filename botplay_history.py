27171eb54daa541c6edde941045255e9e1b58392
EJune <ejunef09@gmail.com>
2022-09-29 21:06:18 +0800

    rename file

---
+++ botplay_history.py
@@ -0,0 +1,11 @@
+class history :
+    list = dict()
+    def add(self,code,piece_position):
+        self.list[code] = piece_position
+    
+    def get_piece_postion(self,code):
+        if code not in self.list :
+            return
+        return self.list[code]
+
+
