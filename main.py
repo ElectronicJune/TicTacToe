e45033b4772b2d6b419b67b8d48305d807fce3b6
EJune <ejunef09@gmail.com>
2022-09-29 21:30:50 +0800

    done game loop

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import display 
 import rules
-import gameplay_history
+import botplay_history
 import brain
+history = botplay_history.history()
 #print title 
 display.print_title()
 
@@ -17,6 +18,7 @@ bot = brain.memory()
 #functions
 def bot_play(code):
     position = bot.react(code)
+    history.add(code,position)
     return code[:int(position)-1] + rules.bot_piece + code[int(position):]
 def player_play(code):
     while True :
@@ -28,15 +30,24 @@ while run_game :
     game_round += 1
     print('ROUND',game_round)
     game_condition = '123456789'
-    
-    if game_round%2 == 0 :
-        # code 
-        pass
     #inner game loop
+    inner_game_round = 1
     while True:
-        display.print_tictactoe(game_condition)
-        #code
-        pass
+        if inner_game_round% 2 == (0 if game_round%2==0 else 1) :
+            game_condition = bot_play(game_condition)
+        else:
+            display.print_tictactoe(game_condition)
+            game_condition = player_play(game_condition)
+        if rules.is_gameover(game_condition):
+            display.print_tictactoe(game_condition)
+            if rules.winner(game_condition)==rules.player_piece:
+                print('YOU WIN')
+            elif rules.winner(game_condition)==rules.bot_piece:
+                print('YOU LOSE')
+            else :
+                print('DRAW')
+            break
+        inner_game_round += 1
 
     run_game = False if input('Play again? [Y/n]: ').lower()=='n' else True
 
