4086d2f3cb7c175a6a6d42196e12b78b1f604f1e
EJune <ejunef09@gmail.com>
2022-09-30 22:22:20 +0800

    change feedback adjust rate

--- brain.py
+++ brain.py
@@ -23,23 +23,23 @@ class memory :
     def learn(self,code,position,winner):
         index = np.where(self.game_code==code)[0][0]
         if winner == rules.bot_piece:
-            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 2
+            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 4
         elif winner == rules.player_piece :
             for i in range(9):
                 if self.game_choice[index][i] != position :
                     if self.game_choice[index][i] != '0' :
                         self.game_choice_p[index][i] += 1
         else :
-            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 1
+            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 2
     def learn_from_player(self,code,position,winner):
         self.add(code)
         index = np.where(self.game_code==code)[0][0]
         if winner == rules.player_piece:
-            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 2
+            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 4
         elif winner == rules.bot_piece :
             for i in range(9):
                 if self.game_choice[index][i] != position :
                     if self.game_choice[index][i] != '0' :
                         self.game_choice_p[index][i] += 1
         else :
-            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 1
+            self.game_choice_p[index][np.where(self.game_choice[index]==position)[0][0]] += 2
