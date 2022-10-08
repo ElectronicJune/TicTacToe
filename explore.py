d35ba8bcff06aa7fe21566a0a3a0df571775c83b
EJune <ejunef09@gmail.com>
2022-10-08 22:09:02 +0800

    fix saving not in pb but in b only error

--- explore.py
+++ explore.py
@@ -1,8 +1,9 @@
 import rules
 import botplay_history
+import playerplay_history
 import brain
 bot_history = botplay_history.history()
-
+player_history = playerplay_history.history()
 # piece info 
 rules.player_piece = 'O'
 rules.bot_piece = 'X' 
@@ -13,9 +14,9 @@ def bot_play(code):
     position = bot.react(code)
     bot_history.add(code,position)
     return code[:int(position)-1] + rules.bot_piece + code[int(position):]
-def bot2_play(code):
+def player_play(code):
     position = bot.react(code)
-    bot_history.add(code,position)
+    player_history.add(code,position)
     return code[:int(position)-1] + rules.player_piece + code[int(position):]
 while input('Explore 1000 rounds? [Y/n] ').lower()=='y' :
     for i in range(1000):
@@ -26,14 +27,14 @@ while input('Explore 1000 rounds? [Y/n] ').lower()=='y' :
             if inner_game_round%2==0:
                 game_condition = bot_play(game_condition)
             else :
-                game_condition = bot2_play(game_condition)
+                game_condition = player_play(game_condition)
+            inner_game_round+=1
         #brain learn
-        round = 0
         for key in bot_history.list :
-            if round%2==0:
-                bot.learn(key,bot_history.list[key],rules.winner(game_condition))
-            else :
-                bot.learn_from_player(key,bot_history.list[key],rules.winner(game_condition))
+            bot.learn(key,bot_history.list[key],rules.winner(game_condition))
+        for key in player_history.list :
+            bot.learn_from_player(key,player_history.list[key],rules.winner(game_condition))
+        player_history.clear()
         bot_history.clear()
         bot.compress_data()
 
